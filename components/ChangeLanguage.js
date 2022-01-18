618cb133e8014d7b756b56a3d58bd2b4f621147b
devlitus <developercarles@gmail.com>
2022-01-18 17:45:47 +0100

    refactor hock useTranslation

--- components/ChangeLanguage.js
+++ components/ChangeLanguage.js
@@ -1,12 +1,11 @@
 import useTranslation from "next-translate/useTranslation";
 import setLanguage from "next-translate/setLanguage";
-import Link from "next/link";
 import i18nConfig from "../i18n.json";
 
 const { locales } = i18nConfig;
 
 export default function ChangeLanguage() {
-  const { t, lang } = useTranslation();
+  const { lang } = useTranslation();
   return locales.map((lng) => {
     if (lng === lang) return null;
     return (
